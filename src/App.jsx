579a7ae321ccfea9c512f92c1cd14055ce06f0db
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2026-01-03 01:17:33 +0800

    ✨ feat(package.json): antd
    - add antd structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
 import { useDispatch, useSelector } from "react-redux";
+import {
+  App as AntdApp,
+  ConfigProvider as DesktopConfigProvider,
+  theme,
+  DatePicker,
+} from "antd";
+import { ConfigProvider as MobileConfigProvider } from "antd-mobile";
+
+import zhCN from "antd/locale/zh_CN";
+import enUS from "antd/locale/en_US";
+import zhCNMobile from "antd-mobile/es/locales/zh-CN";
+import enUSMobile from "antd-mobile/es/locales/en-US";
+
 import { setCurrentPage } from "./redux/navigationSlice";
 import { setLanguage, setTheme } from "./redux/preferenceSlice";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
   const dispatch = useDispatch();
   const currentPage = useSelector((state) => state.navigation.currentPage);
-  const currentLanguage = useSelector((state) => state.preference.language);
-  const currentTheme = useSelector((state) => state.preference.theme);
+  const language = useSelector((state) => state.preference.language);
+  const themeMode = useSelector((state) => state.preference.theme);
 
   return (
-    <>
-      <div className="card">
-        <button onClick={() => dispatch(setCurrentPage("test"))}>
-          Set current page to "test"
-        </button>
-        <p>Current Page: {currentPage}</p>
-        <button onClick={() => dispatch(setLanguage("cn"))}>
-          Set language to "cn"
-        </button>
-        <p>Current Language: {currentLanguage}</p>
-        <button onClick={() => dispatch(setTheme("dark"))}>
-          Set theme to "dark"
-        </button>
-        <p>Current Theme: {currentTheme}</p>
-      </div>
-    </>
+    <DesktopConfigProvider
+      theme={{
+        algorithm:
+          themeMode === "dark" ? theme.darkAlgorithm : theme.defaultAlgorithm,
+      }}
+      locale={language === "cn" ? zhCN : enUS}
+    >
+      <MobileConfigProvider
+        locale={language === "cn" ? zhCNMobile : enUSMobile}
+      >
+        <AntdApp>
+          <div className="card">
+            <button onClick={() => dispatch(setCurrentPage("test"))}>
+              Set current page to "test"
+            </button>
+            <p>Current Page: {currentPage}</p>
+            <button onClick={() => dispatch(setLanguage("cn"))}>
+              Set language to "cn"
+            </button>
+            <p>Current Language: {language}</p>
+            <button onClick={() => dispatch(setTheme("dark"))}>
+              Set theme to "dark"
+            </button>
+            <p>Current Theme: {themeMode}</p>
+            <div style={{ margin: "24px 0" }}>
+              <DatePicker />
+            </div>
+          </div>
+        </AntdApp>
+      </MobileConfigProvider>
+    </DesktopConfigProvider>
   );
 }
 
