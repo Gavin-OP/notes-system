69c2e22c5c51eba5efe1506a1aa20c4bf72d65cb
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2026-01-19 22:57:50 +0800

    🐞 fix(navigationSlice.js): remove useless
    - remove useless redux slice and usage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import navigationReducer from "./navigationSlice";
 import preferenceReducer from "./preferenceSlice";
 import notesIndexReducer from "./notesIndexSlice";
 
 const store = configureStore({
   reducer: {
-    navigation: navigationReducer,
     preference: preferenceReducer,
     notesIndex: notesIndexReducer,
   },
