da6cfa19517b2397eba13b3a07d8b350b61e8699
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2026-01-21 22:40:04 +0800

    (Routes.jsx): child route
    - make note page to be parrent route have multiple child routes

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NoteLayout from "../common/layouts/NoteLayout";
 import NotePage from "../pages/NotePage";
 import HomePage from "../pages/HomePage";
 
@@ -6,7 +7,9 @@ export default function AppRoutes() {
   return (
     <BrowserRouter basename="/notes-system">
       <Routes>
-        <Route path="note/*" element={<NotePage />} />
+        <Route path="note/*" element={<NoteLayout />}>
+          <Route path="*" element={<NotePage />} />
+        </Route>
         <Route path="home" element={<HomePage />} />
         <Route path="*" element={<HomePage />} />
       </Routes>
